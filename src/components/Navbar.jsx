a5c649e05e37f894faf2ec620a66372ffdf63164
Ranjith kumar <ranjith80238023@gmail.com>
2026-03-24 14:13:34 +0530

    resume updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const resumeLink =
-    "https://drive.google.com/file/d/1AbMzhwKfe4DX_UOOTQj_hTBN1wxFQIK4/view?usp=sharing";
+    "https://drive.google.com/file/d/1v8ki187CphuEJhNeaDcZidknwl7nfJ1q/view?usp=sharing";
 
   return (
     <motion.nav
